7a7a55b2df44e4fe8de1cca72c4b153a11b25498
seh <seh@marketlogicsoftware.com>
2020-04-27 19:43:58 +0200

    Fixed file naming

--- src/test/java/com/sergioHerrero/app/MainApplication.java
+++ src/test/java/com/sergioHerrero/app/MainApplication.java
@@ -1,10 +1,10 @@
-package com.example.reacttest;
+package com.sergioHerrero.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReactTestApplicationTests {
+class MainApplication {
 
 	@Test
 	void contextLoads() {
