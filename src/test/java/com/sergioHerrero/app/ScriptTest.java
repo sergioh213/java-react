1ceb3eae5458e72cd58ce76c1817e4446b380784
seh <seh@marketlogicsoftware.com>
2020-05-01 17:29:50 +0200

    Implemented Java tests set-up

---
+++ src/test/java/com/sergioHerrero/app/ScriptTest.java
@@ -0,0 +1,14 @@
+package com.sergioHerrero.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ScriptTest {
+
+	@Test
+	public void ScriptTestsRun() {
+		assertEquals("Tests run successfully", true, true);
+	}
+
+}
